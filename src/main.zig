18e012832b4d875ebf9c68537e327beb1ed8fbc6
Johan Lindskogen <johan.lindskogen@gmail.com>
2024-04-09 20:49:16 +0200

    Initial commit - we got a framebuffer

---
+++ src/main.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+const c = @cImport({
+    @cDefine("FENSTER_HEADER", {});
+    @cInclude("fenster.h");
+});
+
+
+const SCALE: comptime_int = 2;
+const WIDTH: comptime_int = 320;
+const HEIGHT: comptime_int = 240;
+
+pub fn main() !void {
+    var buf: [WIDTH * HEIGHT]u32 = undefined;
+    var f = std.mem.zeroInit(c.fenster, .{
+        .width = WIDTH,
+        .height = HEIGHT,
+        .title = "hello",
+        .buf = &buf[0],
+    });
+    _ = c.fenster_open(&f);
+    defer c.fenster_close(&f);
+
+    var t: u32 = 0;
+    var now: i64 = c.fenster_time();
+    while (c.fenster_loop(&f) == 0) {
+        // Exit when Escape is pressed
+        if (f.keys[27] != 0) {
+            break;
+        }
+        // Render x^y^t pattern
+        for (buf, 0..) |_, i| {
+            buf[i] = @as(u32, @intCast(i % WIDTH)) ^ @as(u32, @intCast(i / HEIGHT)) ^ t;
+        }
+        t +%= 1;
+        // Keep ~60 FPS
+        const diff: i64 = 1000 / 60 - (c.fenster_time() - now);
+        if (diff > 0) {
+            c.fenster_sleep(diff);
+        }
+        now = c.fenster_time();
+    }
+}
