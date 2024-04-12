47a18582a8804c311003c9eb999f5e58647d4d24
Johan Lindskogen <johan.lindskogen@gmail.com>
2024-04-12 18:15:00 +0200

    Add base CPU registers and main run loop

--- src/main.zig
+++ src/main.zig
@@ -9,7 +9,59 @@ const SCALE: comptime_int = 2;
 const WIDTH: comptime_int = 320;
 const HEIGHT: comptime_int = 240;
 
+const Mem = [65_536]u8;
+
+const CPU = struct {
+    pc: u16,
+
+    /// Offset from $0100
+    sp: u8,
+    a: u8,
+    x: u8,
+    y: u8,
+
+    /// N, V, _, B, D, I, Z, C
+    p: u8,
+
+
+    fn jmp(self: *CPU, dst: u16) void {
+        self.pc = dst;
+    }
+
+    fn run(self: *CPU, mem: *Mem) void {
+        switch (mem[self.pc]) {
+            // 0x29 => {
+            //     self.a = self.a & mem[self.pc + 1];
+            //     self.pc += 1;
+            // },
+            0xd8 => {
+
+            },
+            else => {
+                std.debug.print("Unreachable: {x}\n", .{ mem[self.pc] });
+                unreachable;
+            }
+        }
+        self.pc += 1;
+    }
+};
+
 pub fn main() !void {
+    var cpu = CPU { .p = 0, .a = 0, .x = 0, .y = 0, .sp = 0xff, .pc = 0 };
+    var mem: Mem = undefined;
+
+    const text = @embedFile("6502_functional_test.bin");
+
+    std.mem.copyForwards(u8, &mem, text);
+
+    cpu.jmp(0x0400);
+    var pc: u16 = 0;
+    while (cpu.pc != pc) {
+        pc = cpu.pc;
+        cpu.run(&mem);
+        std.debug.print("loop {x}\n", .{ pc });
+    }
+
     var buf: [WIDTH * HEIGHT]u32 = undefined;
     var f = std.mem.zeroInit(c.fenster, .{
         .width = WIDTH,
