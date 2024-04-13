461ce2d735da0b96e4e18671e472652e3440d235
Johan Lindskogen <johan.lindskogen@gmail.com>
2024-04-13 16:32:58 +0200

    Implement some versions of LDA STA LDX TXS & CLD

--- src/cpu.zig
+++ src/cpu.zig
@@ -2,6 +2,16 @@ const AddrMode = @import("addr.zig").AddrMode;
 const Mem = @import("mem.zig").Mem;
 const std = @import("std");
 
+const Flags = packed struct(u8) {
+  negative: bool = false,
+  overflow: bool = false,
+  _padding: u1 = 0,
+  break_command: bool = false,
+  decimal_mode: bool = false,
+  interrupt_disable: bool = false,
+  zero: bool = false,
+  carry: bool = false,
+};
 
 pub const CPU = struct {
   pc: u16,
@@ -13,10 +23,12 @@ pub const CPU = struct {
   y: u8,
 
   /// N, V, _, B, D, I, Z, C
-  p: u8,
+  p: Flags,
+
+  cycles: u32,
 
   pub fn init() CPU {
-    return .{ .p = 0, .a = 0, .x = 0, .y = 0, .sp = 0xff, .pc = 0 };
+    return .{ .p = Flags {}, .a = 0, .x = 0, .y = 0, .sp = 0xff, .pc = 0, .cycles = 0 };
   }
 
 
@@ -25,6 +37,58 @@ pub const CPU = struct {
       .absolute => |v| v,
       else => unreachable,
     };
+  }
+
+  fn ldx(self: *CPU, dst: AddrMode) void {
+    self.x = switch (dst) {
+      .immediate => |v| v,
+      else => unreachable,
+    };
+
+    self.cycles += switch (dst) {
+      .immediate => 2,
+      else => unreachable,
+    };
+
+    self.p.zero = self.x == 0;
+    self.p.negative = @clz(self.x) == 0;
+  }
+
+  fn lda(self: *CPU, dst: AddrMode) void {
+    self.a = switch (dst) {
+      .immediate => |v| v,
+      else => unreachable,
+    };
+
+    self.cycles += switch (dst) {
+      .immediate => 2,
+      else => unreachable,
+    };
+
+    self.p.zero = self.a == 0;
+    self.p.negative = @clz(self.a) == 0;
+  }
+
+  fn sta(self: *CPU, mem: *Mem, dst: AddrMode) void {
+    const addr = switch (dst) {
+      .absolute => |v| v,
+      else => unreachable,
+    };
+
+    mem[addr] = self.a;
+
+    self.cycles += switch (dst) {
+      .absolute => 4,
+      else => unreachable,
+    };
+
+    // No flags affected
+  }
+
+  fn txs(self: *CPU) void {
+    self.sp = self.x;
+    self.cycles += 2;
+    // No flags affected
   }
 
   pub fn run(self: *CPU, mem: *Mem) void {
@@ -42,12 +106,29 @@ pub const CPU = struct {
       //     self.a = self.a & mem[self.pc + 1];
       //     self.pc += 1;
       // },
+      0x9a => {
+        // TXS - Transfer X to Stack Pointer
+        self.txs();
+      },
+      0x8d => {
+        // STA - Store Accumulator
+        self.sta(mem, .{ .absolute = mem[self.pc + 1]});
+        self.pc += 1;
+      },
+      0xa9 => {
+        // LDA - Load Accumulator
+        self.lda(.{ .immediate = mem[self.pc + 1] });
+        self.pc += 1;
+      },
       0xa2 => {
-
+        // LDX - Load X Register
+        self.ldx(.{ .immediate = mem[self.pc + 1] });
+        self.pc += 1;
       },
       0xd8 => {
         // CLD - Clear Decimal Mode
-
+        self.p.decimal_mode = false;
+        self.cycles += 2;
       },
       else => {
         std.debug.print("Unreachable: {x}\n", .{ mem[self.pc] });
@@ -57,3 +138,6 @@ pub const CPU = struct {
     self.pc += 1;
   }
 };
+
+
+
